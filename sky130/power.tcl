2ab2b75456409f3e9d9ebe721e020aa744b294a2
G S bharath <76478624+bharath19-gs@users.noreply.github.com>
2023-07-17 23:31:57 +0530

    Delete sky130 directory

--- sky130/power.tcl
+++
@@ -1,9 +0,0 @@
-read_liberty sky130_fd_sc_hd__tt_025C_1v80.lib
-read_verilog 1_synth.v
-link_design gcd
-create_clock -name clk -period 10 {clk}
-set_input_delay -clock clk 0.1 {req_msg req_val reset resp_rdy}
-set_power_activity -input -activity 0.1
-set_power_activity -input_port reset -activity 0
-report_power > power_sky.txt
-exit
--- sky130/timing.tcl
+++
@@ -1,7 +0,0 @@
-read_liberty sky130_fd_sc_hd__tt_025C_1v80.lib
-read_verilog 1_synth.v
-link_design gcd
-create_clock -name clk -period 10 {clk}
-set_input_delay -clock clk 0.1 {req_msg req_val reset resp_rdy}
-report_checks -path_delay min_max > timing_details_sky130.txt
-exit
